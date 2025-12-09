3acea1249bc931ed8c7bdfd7247d65dafd5856b3
Arya Gandhi <aryagandhi80@icloud.com>
2025-12-09 15:18:20 -0800

    Delete cont_stick_event.m

--- cont_stick_event.m
+++
@@ -1,8 +0,0 @@
-function [value, isterminal, direction] = cont_stick_event(t,s)
-    dx_s = dpSt_gen(s) ; 
-    
-    value = dx_s ; 
-    isterminal = 1 ; 
-    direction = 0 ;  %The direction of slip doesnt matter?
-end
-
